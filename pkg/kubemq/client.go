ccb08e78e1ad5affba367817196bfc059249f49e
kubemq <lior.nabat@kubemq.io>
2020-11-09 20:07:49 +0200

    fix load of authentication file

--- pkg/kubemq/client.go
+++ pkg/kubemq/client.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"strings"
+
+	//"fmt"
 	"github.com/google/uuid"
 	"github.com/kubemq-io/kubemq-go"
 	"github.com/kubemq-io/kubemqctl/pkg/config"
-	"io/ioutil"
+	//"io/ioutil"
 )
+
+var authKey = `eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJDbGllbnRJRCI6ImNsaWVudF9pZCIsIlN0YW5kYXJkQ2xhaW1zIjp7ImV4cCI6MTYwNTQzMjgyMX19.lPZpTxTBWnNa0RUuMqJuJR1CIKQXFUssg5SRv_0QmeM-SsVt8qFH42gdM5-t4Z4NgoQSDrxuFy81eoD1ZemiTilvKlpqlW3YPxVMclIfOdH7UueB_3lM5ZWXFWwXnzkjM7ngm-NMm1RoDKFhA7AXzxHFDegXp4wn51jxfRYX3j-HHwLRl-nHZQITjFYZrdHHhKraeYAj_X9KP8vOyOsTFEN-oiLNSj2Kr4iBnUDHmapaCqH683o2xPb39RFaQC8eZbO1FsCGbvVyo4ToLMzrv3FIfarMG778Jwwm8EaBaajqR9kn96jx7gxWPCpi-ECsz9KqhyVju86jXol-UmAPwA`
 
 func GetKubemqClient(ctx context.Context, transport string, cfg *config.Config) (*kubemq.Client, error) {
 	clientId := uuid.New().String()
@@ -22,7 +28,11 @@ func GetKubemqClient(ctx context.Context, transport string, cfg *config.Config) 
 		if err != nil {
 			return nil, fmt.Errorf("error loading Authentication token file: %s", err.Error())
 		}
-		opts = append(opts, kubemq.WithAuthToken(string(data)))
+
+		key := string(data)
+		key = strings.Replace(key, "\r\n", "", -1)
+
+		opts = append(opts, kubemq.WithAuthToken(key))
 	}
 	if cfg.CertFile != "" {
 		opts = append(opts, kubemq.WithCredentials(cfg.CertFile, ""))
