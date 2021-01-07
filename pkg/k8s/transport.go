1c895c9dba51b64115757ee539f4ab6a0d283b10
kubemq <lior.nabat@kubemq.io>
2021-01-07 11:23:15 +0200

    fix send queue messages - fails

--- pkg/k8s/transport.go
+++ pkg/k8s/transport.go
@@ -93,69 +93,6 @@ func SetTransport(ctx context.Context, cfg *config.Config) error {
 	}()
 	return nil
 }
-func SetTransportWithService(ctx context.Context, cfg *config.Config) error {
-	if !cfg.AutoIntegrated {
-		return nil
-	}
-	utils.Print("connecting to kubernetes cluster... ")
-	c, err := client.NewClient(cfg.KubeConfigPath)
-	if err != nil {
-		return err
-	}
-	podNameSpace, podName, err := GetRunningClusterPod(c, cfg.CurrentNamespace, cfg.CurrentStatefulSet)
-	if err != nil {
-		return err
-	}
-	freePorts, err := getFreePorts(3)
-	if err != nil {
-		return err
-	}
-
-	ports := []string{
-		fmt.Sprintf("%d:%d", freePorts[0], cfg.GrpcPort),
-		fmt.Sprintf("%d:%d", freePorts[1], cfg.RestPort),
-		fmt.Sprintf("%d:%d", freePorts[2], cfg.ApiPort),
-	}
-	cfg.GrpcPort = freePorts[0]
-	cfg.RestPort = freePorts[1]
-	cfg.ApiPort = freePorts[2]
-
-	stopCh := make(chan struct{})
-	outCh, errOutCh := make(chan string, 1), make(chan string, 1)
-	err = c.ForwardPortsToService(podNameSpace, "kubemq-cluster-grpc", []string{"50000"}, stopCh, outCh, errOutCh)
-	if err != nil {
-		return err
-	}
-	select {
-	case <-outCh:
-		utils.Printlnf("->  connected to %s/%s at gRPC Port %s Rest Port %s Api Port %s, ok", podNameSpace, podName, ports[0], ports[1], ports[2])
-	case errstr := <-errOutCh:
-		return fmt.Errorf(errstr)
-
-	case <-time.After(30 * time.Second):
-		return fmt.Errorf("timeout during setting of transport layer to kubernetes cluster")
-
-	case <-ctx.Done():
-		close(stopCh)
-		return nil
-	}
-
-	go func() {
-		for {
-			select {
-			case <-outCh:
-
-			case errstr := <-errOutCh:
-				utils.CheckErr(errors.New(errstr))
-				return
-			case <-ctx.Done():
-				close(stopCh)
-				return
-			}
-		}
-	}()
-	return nil
-}
 func possibleMap(name string) map[string]string {
 	m := map[string]string{}
 	for i := 0; i < 16; i++ {
